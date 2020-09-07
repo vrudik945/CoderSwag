8a842a6fdf43129a71760c68fa3c301aa74706ef
vrudik945 <vrudik945@gmail.com>
2020-09-07 16:16:57 +0300

    Old way

--- app/src/main/java/com/v_rudik/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/v_rudik/coderswag/Adapters/CategoryAdapter.kt
@@ -15,20 +15,30 @@ class CategoryAdapter(context: Context, categories: List<Category>):  BaseAdapte
     val context = context
     val categories = categories
 
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
         val categoryView: View
+        val holder: ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
 
         val categorie = categories[position]
 
         val resourceId = context.resources.getIdentifier(categorie.image, "drawable",context.packageName)
-        categoryImage.setImageResource(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
 
-        categoryName.text = categorie.title
+        holder.categoryName?.text = categorie.title
 
 
         return categoryView
@@ -44,6 +54,11 @@ class CategoryAdapter(context: Context, categories: List<Category>):  BaseAdapte
 
     override fun getItemId(position: Int): Long {
         return 0
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 
 
--- app/src/main/java/com/v_rudik/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/v_rudik/coderswag/Controller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.v_rudik.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.v_rudik.coderswag.Adapters.CategoryAdapter
 import com.v_rudik.coderswag.R
 import com.v_rudik.coderswag.Services.DataService
@@ -18,5 +19,11 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this,DataService.categories)
 
         categoryListView.adapter = adapter
+
+//        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+//            val category = DataService.categories[i]
+//            Toast.makeText(this,"You clicked on the ${category.title} cell", Toast.LENGTH_SHORT)
+//
+        }
     }
 }
